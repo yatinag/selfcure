445cef67a0905b84e8dc803972bf808b81abfa24
Yeti <YatinAgarwal@yatins-MacBook-Air.local>
2016-05-27 12:30:24 +0530

    Added New Files

--- selfcure/main.py
+++ selfcure/main.py
@@ -1,30 +1,28 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from web_template import *
+"""`main` is the top level module for your Flask application."""
 
-class MainHandler(Handler):
-    def get(self):
-    	self.render('index.html')
-        #self.response.write('Hello world!')
+# Import the Flask Framework
+from flask import Flask
+from flask import render_template
 
-class SignupHandler(Handler):
-	def get(self):
-		self.render('form.html')
+app = Flask(__name__, static_url_path="", static_folder="static")
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+#app.static_url_path('public')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler), ('/start', SignupHandler),
-], debug=True)
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+#    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
